aafa6229f49b58cb1ebd9947820cd2024aa4e319
unknown <Gilbert@.(none)>
2010-03-03 18:17:00 +0800

    Modified   src/remote/obj/DisconnHandler.java
    Signed-off-by:  <Gilbert>

--- src/remote/obj/DisconnHandler.java
+++ src/remote/obj/DisconnHandler.java
@@ -3,5 +3,5 @@ package remote.obj;
 import java.rmi.*;
 
 public interface DisconnHandler extends Remote {
-	public void logout(String username, byte[] msgCipher) throws RemoteException;
+	public boolean logout(String username, byte[] msgCipher) throws RemoteException;
 }
