5a37a83dad4ead9e6674b6c808b40f86e0b35ad9
unknown <Gilbert@.(none)>
2010-03-22 14:20:15 +0800

    change some abstract method.
    Modified   src/remote/obj/AuthHandler.java
    
    Signed-off-by:  <Gilbert>

--- src/remote/obj/AuthHandler.java
+++ src/remote/obj/AuthHandler.java
@@ -1,9 +1,13 @@
 package remote.obj;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.sql.ResultSet;
 
 
 public interface AuthHandler extends Remote {
 
 	public byte[] authenticate(String username, byte[]HEPwd) throws RemoteException;
+	public byte[] getEncryptedSessionKey(String username) throws RemoteException;
+	public ResultSet getPrivilege(String username) throws RemoteException;
+	
 }
