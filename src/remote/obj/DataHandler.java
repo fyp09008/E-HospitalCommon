9091d3b48b6d3ac0b94e3dd683996e44af02d607
unknown <Gilbert@.(none)>
2010-03-24 11:40:43 +0800

    change update and insert function to return int.
    Modified   src/remote/obj/DataHandler.java
    
    Signed-off-by:  <Gilbert>

--- src/remote/obj/DataHandler.java
+++ src/remote/obj/DataHandler.java
@@ -7,8 +7,8 @@ public interface DataHandler extends Remote {
 
 	public byte[] query(String sessionID, byte[] queryStmt) throws RemoteException;
 
-	public byte[] insert(String sessionID, byte[] insertStmt) throws RemoteException;
+	public int insert(String sessionID, byte[] insertStmt) throws RemoteException;
 	
-	public byte[] update(String sessionID, byte[] updateStmt) throws RemoteException;
+	public int update(String sessionID, byte[] updateStmt) throws RemoteException;
 	
 }
