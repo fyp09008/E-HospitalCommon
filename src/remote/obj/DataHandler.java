1a9d338e4d71e347bbe1ec1774fc9f9ca1bd06da
unknown <mc@.(none)>
2010-03-30 20:59:20 +0800

    Modified   .classpath Modified   .gitignore Modified   .project Modified   .settings/org.eclipse.jdt.core.prefs Modified   src/remote/obj/ClientCallback.java Modified   src/remote/obj/DataHandler.java
    Signed-off-by:  <mc>

--- src/remote/obj/DataHandler.java
+++ src/remote/obj/DataHandler.java
@@ -7,8 +7,12 @@ public interface DataHandler extends Remote {
 
 	public byte[] query(String sessionID, byte[] queryStmt) throws RemoteException;
 
+	public byte[] query(String sessionID, byte[] queryStmt, byte[] param) throws RemoteException;
+
 	public int insert(String sessionID, byte[] insertStmt) throws RemoteException;
 	
 	public int update(String sessionID, byte[] updateStmt) throws RemoteException;
+	
+	public int update(String sessionID, byte[] updateStmt, byte[] param) throws RemoteException;
 	
 }
