0e385b2da5ad31b4d2cc64b76a52b7dee5fa9429
unknown <Gilbert@.(none)>
2010-04-09 21:23:35 +0800

    Modified   src/remote/obj/EmergencyAccessHandler.java
    Signed-off-by:  <Gilbert>

--- src/remote/obj/EmergencyAccessHandler.java
+++ src/remote/obj/EmergencyAccessHandler.java
@@ -1,7 +1,8 @@
 package remote.obj;
 
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 
 public interface EmergencyAccessHandler extends Remote {
-	public int emergencyAccess(byte[] username, byte[] emergencyUser, byte[] emergencyPwd, int tmpCardNum);
+	public int emergencyAccess(byte[] username, byte[] emergencyUser, byte[] emergencyPwd, int tmpCardNum) throws RemoteException;
 }
