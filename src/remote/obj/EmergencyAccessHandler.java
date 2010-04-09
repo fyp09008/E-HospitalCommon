b033523bac56ebdb5041e533abf31247a768f3c8
unknown <Chris@.(none)>
2010-04-09 21:25:39 +0800

    Modified   src/remote/obj/EmergencyAccessHandler.java
    Signed-off-by:  <cc>

--- src/remote/obj/EmergencyAccessHandler.java
+++ src/remote/obj/EmergencyAccessHandler.java
@@ -4,5 +4,5 @@ import java.rmi.Remote;
 
 public interface EmergencyAccessHandler extends Remote {
 	public static String msg = "E-Hospital must A.";
-	public int emergencyAccess(String username, String emergencyUser, byte[] emergencyPwd, int tmpCardNum);
+	public int emergencyAccess(byte[]username, byte[] emergencyUser, byte[] emergencyPwd, int tmpCardNum);
 }
